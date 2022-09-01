969820c55e8533ca27b640be2fe0673b47324fa1
Ahmed Raja <51364583+ahmedraja26@users.noreply.github.com>
2022-09-01 15:58:55 +0200

    Update work experience
    Added new work experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,27 +1,33 @@
 export const experienceData = [
   {
     id: 1,
+    company: "Amazon",
+    jobtitle: "L4 Software Development Engineer",
+    startYear: "2022 Sep",
+    endYear: "Current",
+  },{
+    id: 2,
     company: "IX Online",
     jobtitle: "Junior Full Stack Developer",
     startYear: "2021 Dec",
-    endYear: "Current",
+    endYear: "2022 Aug",
   },
   {
-    id: 2,
+    id: 3,
     company: "University of Kwa-Zulu Natal",
     jobtitle: "Post Graduate Assistant",
     startYear: "2021 Mar",
     endYear: "2021 Nov",
   },
   {
-    id: 3,
+    id: 4,
     company: "Amazon",
     jobtitle: "Intern Software Development Engineer",
     startYear: "2021 Jan",
     endYear: "2021 Mar",
   },
   {
-    id: 3,
+    id: 5,
     company: "University of Kwa-Zulu Natal",
     jobtitle: "Post Graduate Assistant",
     startYear: "2020 Jan",
